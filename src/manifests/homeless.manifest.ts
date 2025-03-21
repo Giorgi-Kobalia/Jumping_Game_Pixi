07d29e886fc0bed86bf22479b71a13cdc30d23a6
Giorgi Kobalia <gqobalia00@gmail.com>
2025-03-22 00:12:13 +0300

    Update image paths in manifests to use relative URLs

--- src/manifests/homeless.manifest.ts
+++ src/manifests/homeless.manifest.ts
@@ -1,10 +1,10 @@
 export const homeless_manifest = {
-  homeless_dead: "/images/homeless/dead.json",
-  homeless_jump: "/images/homeless/jump.json",
-  homeless_run: "/images/homeless/run.json",
-  homeless_drink: "/images/homeless/drink.json",
-  homeless_walk: "/images/homeless/walk.json",
-  homeless_special: "/images/homeless/special.json",
-  homeless_idle: "/images/homeless/idle.json",
-  homeless_hurt: "/images/homeless/hurt.json",
+  homeless_dead: "./images/homeless/dead.json",
+  homeless_jump: "./images/homeless/jump.json",
+  homeless_run: "./images/homeless/run.json",
+  homeless_drink: "./images/homeless/drink.json",
+  homeless_walk: "./images/homeless/walk.json",
+  homeless_special: "./images/homeless/special.json",
+  homeless_idle: "./images/homeless/idle.json",
+  homeless_hurt: "./images/homeless/hurt.json",
 };
